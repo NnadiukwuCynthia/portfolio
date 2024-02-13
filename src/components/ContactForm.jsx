327c1696c70c9e6f88b9a1baa00cdf0699a67c7a
Cynthia Nnadiuwku <cynthiaamaran@gmail.com>
2024-02-13 22:54:37 +0100

    Task Working on  contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,24 +1,89 @@
 import { useState } from "react"
-import { FormControl, FormLabel, Input, FormErrorMessage, FormHelperText,} from "@chakra-ui/react"
+import { FormControl, FormLabel, Input, FormErrorMessage, NumberInputField, NumberInput, Box } from "@chakra-ui/react"
 
 const ContactForm = () => {
-    const [input, setInput] = useState('')
+    const [name, setName] = useState('');
+    const [nameError, setNameError] = useState(false);
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [phoneNumberError, setPhoneNumberError] = useState(false);
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState(false);
+    const [subject, setSubject] = useState('');
+    const [subjectError, setSubjectError] = useState(false);
 
-  const handleInputChange = (e) => setInput(e.target.value)
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
 
-  const isError = input === ''
+    const handlePhoneNumberChange = (event) => {
+        setPhoneNumber(event.target.value);
+    };
+
+    const handleEmailChange = (event) => { 
+        setEmail(event.target.value);
+    };
+
+    const handleSubjectChange = (event) => { 
+        setSubject(event.target.value);
+    };
+    
+    const handleNameValidation = () => {
+        if (name.trim() === '') {
+        setNameError(true);
+        } else {
+        setNameError(false);
+        }
+    };
+
+    const handlePhoneNumberValidation = () => {
+       if (phoneNumber.trim() === '') {
+            setPhoneNumberError(true);
+            } else {
+            setPhoneNumberError(false);
+        }
+    }
+
+    const handleEmailValidation = () => {
+        if (email.trim() === '') {
+            setEmailError(true);
+            } else {
+            setEmailError(false);
+        }
+    }
+    const handleSubjectValidation = () => {
+        if (subject.trim() === '') {
+            setSubjectError(true);
+            } else {
+            setSubjectError(false);
+        }
+    }
+
   return (
-    <FormControl  w='40%' m='20px 10px' py='30px' borderRadius='xl' boxShadow='-1px 2px 10px #1d1d20' bg='#171718'>
-            <FormLabel p='20px 30px 5px' textTransform='uppercase' color='grey'>Name</FormLabel>
-            <Input mx='30px' w='90%' h='4rem' bg='#1d1d20' color='white' px='20px' fontWeight='bold' border='1px solid #171718' boxShadow='xl' type='text' value={input} onChange={handleInputChange}/>
-            {!isError ? (
-            <FormHelperText color='white' px='30px'>
-                Enter Correct Mail Address.
-            </FormHelperText>
-        ) : (
-        <FormErrorMessage>Email is required.</FormErrorMessage>
-      )}
-        </FormControl>
+    <Box w='40%' m='20px 10px' py='30px' borderRadius='xl' boxShadow='-1px 2px 10px #1d1d20' bg='#171718'>
+    <FormControl >
+         <FormLabel p='20px 30px 5px' textTransform='uppercase' color='gray'>Name</FormLabel>
+            <Input _hover={{border:'3px solid crimson'}} focusBorderColor='tranparent' mx='30px' w='90%' h='4rem' bg='#1d1d20' color='white' px='20px' fontWeight='bold' border='1px solid #171718' boxShadow='xl' type='text' value={name} onChange={handleNameChange} onBlur={handleNameValidation} />
+            {nameError && <FormErrorMessage>Name is Required.</FormErrorMessage>}
+    </FormControl>
+    <FormControl>    
+            <FormLabel p='20px 30px 5px' textTransform='uppercase' color='gray'>Phone Number</FormLabel>
+            <NumberInput>
+                <NumberInputField _hover={{border:'3px solid crimson'}} focusBorderColor='none' mx='30px' w='90%' h='4rem' bg='#1d1d20' color='white' px='20px' fontWeight='bold' border='1px solid #171718' boxShadow='xl' type='number' value={phoneNumber} onChange={handlePhoneNumberChange} onBlur={handlePhoneNumberValidation}/>
+                {phoneNumberError && <FormErrorMessage>Phone Number is Required.</FormErrorMessage>}
+            </NumberInput>
+    </FormControl>
+    <FormControl>                     
+            <FormLabel p='20px 30px 5px' textTransform='uppercase'  color='gray'>Email</FormLabel>
+            <Input _hover={{border:'3px solid crimson'}} focusBorderColor='tranparent' mx='30px' w='90%' h='4rem' bg='#1d1d20' color='white' px='20px' fontWeight='bold' border='1px solid #171718' boxShadow='xl' type='email' value={email} onChange={handleEmailChange} onBlur={handleEmailValidation} />
+            {emailError && <FormErrorMessage>Email is Required.</FormErrorMessage>}
+              
+    </FormControl>
+    <FormControl >
+         <FormLabel p='20px 30px 5px' textTransform='uppercase' color='gray'>Subject</FormLabel>
+            <Input _hover={{border:'3px solid crimson'}} focusBorderColor='tranparent' mx='30px' w='90%' h='4rem' bg='#1d1d20' color='white' px='20px' fontWeight='bold' border='1px solid #171718' boxShadow='xl' type='text' value={subject} onChange={handleSubjectChange} onBlur={handleSubjectValidation} />
+            {subjectError && <FormErrorMessage>Subject is Required.</FormErrorMessage>}
+    </FormControl> 
+    </Box>
   )
 }
 
