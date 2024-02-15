ca02ae670fb49d5e1037a5dc74f843afff8340dd
Cynthia Nnadiuwku <cynthiaamaran@gmail.com>
2024-02-15 19:43:45 +0100

    Task: Worked on the media Query

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -3,7 +3,7 @@ import About from "../components/About"
 
 const AboutMe = () => {
   return (
-    <Box w='100%' h='100vh' pt='6rem' bg='#000000'>
+    <Box w='100%' h={{base: '120vh', sm: '100vh', md: '100vh', lg: '100vh'}} pt='6rem' bg='#000000'>
         <About/>
     </Box>
   )
